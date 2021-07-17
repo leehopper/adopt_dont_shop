7b4c9e0941766f295ee809b06886b273d733224c
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-17 11:15:30 -0600

    Add validation test for status enum

--- spec/models/applications_spec.rb
+++ spec/models/applications_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Application, type: :model do
     it { should have_many(:pet_applications) }
     it { should have_many(:pets).through(:pet_applications) }
   end
+
+  describe 'validations' do
+    it { should define_enum_for(:status).with_values([:in_progress, :pending, :accepted, :rejected]) }
+  end
 end
