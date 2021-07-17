8aa9e016c2fc21f5ed1a19ecc5b218faf27b1c45
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-17 16:09:42 -0600

    Add default value to description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_163407) do
+ActiveRecord::Schema.define(version: 2021_07_17_220346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_07_17_163407) do
     t.string "city"
     t.string "state"
     t.integer "zip_code"
-    t.text "description"
+    t.string "description", default: ""
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
