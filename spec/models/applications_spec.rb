1f2ded863f85cbd13d9258d577a3e9ef8e4aea4c
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-17 18:09:37 -0600

    Build test for validations

--- spec/models/applications_spec.rb
+++ spec/models/applications_spec.rb
@@ -7,6 +7,15 @@ RSpec.describe Application, type: :model do
   end
 
   describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:street_address) }
+    it { should validate_presence_of(:city) }
+    it { should validate_presence_of(:state) }
+    it { should validate_presence_of(:zip_code) }
+    it { should validate_numericality_of(:zip_code) }
+  end
+
+  describe 'enum' do
     it { should define_enum_for(:status).with_values([:in_progress, :pending, :accepted, :rejected]) }
   end
 end
