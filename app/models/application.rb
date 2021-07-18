fcbab9a568478aa21e7638ba74f7ac7a35443067
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-17 18:08:38 -0600

    Add validates declarations

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,6 @@
 class Application < ApplicationRecord
+  validates_presence_of :name, :street_address, :city, :state, :zip_code
+  validates :zip_code, numericality: {only_integer: true, greater_than: 1, less_than: 99999 }
   has_many :pet_applications
   has_many :pets, through: :pet_applications
   enum status: [ :in_progress, :pending, :accepted, :rejected ]
