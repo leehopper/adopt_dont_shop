658a9f9a6a800721ba4e6e13d26b6db9fad43d97
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-19 22:55:21 -0600

    Build enum test for application status

--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe PetApplication, type: :model do
     it { should belong_to :pet }
     it { should belong_to :application }
   end
+
+  describe 'enum' do
+    it { should define_enum_for(:approval_status).with_values([:pending, :approved, :denied]) }
+  end
 end
