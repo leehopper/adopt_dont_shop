7142d23326bbe36c33d8240656d1373a2dca080a
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-20 20:18:56 -0600

    Add function to approve or deny pet depending on params

--- app/controllers/admin/pet_applications_controller.rb
+++ app/controllers/admin/pet_applications_controller.rb
@@ -2,11 +2,12 @@ class Admin::PetApplicationsController < ApplicationsController
   def update
     pet_application = PetApplication.locate_record(params[:id], params[:pet])
 
-    if pet_application.approve
-      redirect_to "/admin/applications/#{params[:id]}"
-    else
-      redirect_to "/admin/applications/#{params[:id]}"
-      flash[:alert] = "Error: #{error_message(application.errors)}"
-    end 
+    if params[:status] == 'approved'
+      pet_application.approve
+    elsif params[:status] == 'denied'
+      pet_application.deny
+    end
+
+    redirect_to "/admin/applications/#{params[:id]}"
   end
 end
