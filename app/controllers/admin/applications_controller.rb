5c982871be5cb0dea9bc3894b09f3097723b9871
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-20 18:18:55 -0600

    Build temp method call for model build

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -2,5 +2,6 @@ class Admin::ApplicationsController < ApplicationsController
   def show
     @application = Application.find(params[:id])
     @pets = @application.pets
+    # @approved_pets = @application.approved_pets
   end
 end
