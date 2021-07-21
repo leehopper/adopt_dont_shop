b2fcd39d3742bf7b143e0e2273bafab4546c8068
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-20 18:17:55 -0600

    Build methods for approve and locate record

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -3,4 +3,12 @@ class PetApplication < ApplicationRecord
   belongs_to :application
 
   enum approval_status: [ :pending, :approved, :denied ]
+
+  def self.locate_record(application_id, pet_id)
+    where(application_id: application_id, pet_id: pet_id).first
+  end
+
+  def approve
+    update(approval_status: 'approved')
+  end
 end
