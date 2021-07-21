a43eea9e622ed4d241e5f10f85eecdd4874f33c3
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-20 22:21:09 -0600

    Build tests and update show to only display approve or deny pets when app pending

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -41,6 +41,7 @@ RSpec.describe 'the admin application' do
     expect(page).to have_content(@application.zip_code)
     expect(page).to have_content(@application.status)
     expect(page).to have_content(@application.description)
+    expect(page).to have_content('Status: pending')
   end
 
   it 'displays all pets for the application' do
@@ -116,5 +117,80 @@ RSpec.describe 'the admin application' do
       expect(page).to have_selector(:link_or_button, "Approve #{@pet_2.name}")
       expect(page).to have_selector(:link_or_button, "Deny #{@pet_2.name}")
     end
+  end
+
+  it 'application status accepted when all pets approved' do
+    visit "/admin/applications/#{@application.id}"
+
+    within("#pet-#{@pet_1.id}") do
+      click_button "Approve #{@pet_1.name}"
+    end
+
+    expect(page).to have_content('Status: pending')
+
+    within("#pet-#{@pet_2.id}") do
+      click_button "Approve #{@pet_2.name}"
+    end
+
+    expect(page).to have_content('Status: accepted')
+  end
+
+  it 'application status rejected when a pets denied' do
+    @application.pets << @pet_3
+
+    visit "/admin/applications/#{@application.id}"
+
+    within("#pet-#{@pet_1.id}") do
+      click_button "Approve #{@pet_1.name}"
+    end
+
+    expect(page).to have_content('Status: pending')
+
+    within("#pet-#{@pet_2.id}") do
+      click_button "Deny #{@pet_2.name}"
+    end
+
+    expect(page).to have_content('Status: pending')
+
+    within("#pet-#{@pet_3.id}") do
+      click_button "Approve #{@pet_3.name}"
+    end
+
+    expect(page).to have_content('Status: rejected')
+  end
+
+  it 'approve or deny pets section not visible if app approved' do
+    visit "/admin/applications/#{@application.id}"
+
+    click_button "Approve #{@pet_1.name}"
+    click_button "Approve #{@pet_2.name}"
+
+    expect(page).to_not have_content('Pets to approve or deny')
+    expect(page).to_not have_content(@pet_1.name)
+    expect(page).to_not have_content(@pet_2.name)
+  end
+
+  it 'approve or deny pets section not visible if app rejected' do
+    visit "/admin/applications/#{@application.id}"
+
+    click_button "Deny #{@pet_1.name}"
+    click_button "Deny #{@pet_2.name}"
+
+    expect(page).to_not have_content('Pets to approve or deny')
+    expect(page).to_not have_content(@pet_1.name)
+    expect(page).to_not have_content(@pet_2.name)
+  end
+
+  it 'approve or deny pets section not visible if app in_progress' do
+    application_2 = Application.create!(name: 'Natalie', street_address: '1234 Random St', city: 'Englewood', state: 'CO', zip_code: '80205')
+
+    application_2.pets << @pet_1
+    application_2.pets << @pet_2
+
+    visit "/admin/applications/#{application_2.id}"
+
+    expect(page).to_not have_content('Pets to approve or deny')
+    expect(page).to_not have_content(@pet_1.name)
+    expect(page).to_not have_content(@pet_2.name)
   end
 end
