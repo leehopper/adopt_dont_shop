9077f721ef2b8c2d17d79ccc5dbb7f2c1d519f6d
Lee Hopper <hopper.lee.j@gmail.com>
2021-07-22 09:57:40 -0600

    Heroku updates

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,5 @@
 require_relative 'boot'
 
-config.assets.initialize_on_precompile = false
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -12,7 +10,7 @@ module AdoptDontShop
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
